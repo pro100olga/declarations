229d7ac90cd5c34a22ad68084cb6eb1b5933928c
Olga Makarova <pro100olga@gmail.com>
2020-02-23 10:14:06 +0200

    Update declarations_extract_banks_edrpou_and_names.py

--- bi/red_flags/declarations_extract_banks_edrpou_and_names.py
+++ bi/red_flags/declarations_extract_banks_edrpou_and_names.py
@@ -1,4 +1,3 @@
-
 import requests
 import string
 import re
@@ -6,39 +5,60 @@ import pandas as pd
 
 ####################### BANKS NAMES AND EDRPOU CODES
 
-# We will take data about banks from 2 APIs (bank.gov.ua and nbu.rocks) and extract EDRPOU codes and banks' names
+# We will take data about banks from 2 APIs (data.gov.ua and nbu.rocks) and extract EDRPOU codes and banks' names
 
 banks1 = requests.get("https://bank.gov.ua/NBU_BankInfo/get_data_branch?&json").json()
 banks2 = requests.get("http://nbu.rocks/jsons/complete.json").json()
 
 # banks1 = banks1['rcukru']['rowrcukru'] # extract list of banks
 
-# Create a list of EDRPOU codes
+# Create a list of EDRPOU codes and banks names
 
 banks_edrpou = []
 banks_names = []
 
 for i in range(len(banks1)): # add all codes from 1st list, convert them to single type - string
 
-    # Long bank name 
-    banks_names.append(re.sub(' +',' ', banks1[i]['N_GOL'].lower().translate(str.maketrans('', '', string.punctuation)).strip())) # remove all punctuation, double spaces and trail spaces, lowercase
+    # remove all punctuation, double spaces and trail spaces, lowercase
+    
+    # Long bank name
+    
+    bname = banks1[i]['N_GOL'].lower()
+    for c in string.punctuation:
+        bname = bname.replace(c, " ")
+    bname = re.sub(' +',' ', bname.strip())
+    
+    banks_names.append(bname) 
       
     # Short bank name 
-    banks_names.append(re.sub(' +',' ', banks1[i]['SHORTNAME'].lower().translate(str.maketrans('', '', string.punctuation)).strip())) # remove all punctuation, double spaces and trail spaces, lowercase
+    
+    bname = banks1[i]['SHORTNAME'].lower()
+    for c in string.punctuation:
+        bname = bname.replace(c, " ")
+    bname = re.sub(' +',' ', bname.strip())
+    
+    banks_names.append(bname)  
     
     # EDRPOU code   
+    
     banks_edrpou.append(str(banks1[i]['KOD_EDRPOU']))
 
 
 for i in range(len(banks2)): # add all codes from 2nd list, convert them to single type - string
 
     # Bank name 
-    banks_names.append(re.sub(' +',' ', banks2[i]['Ліцензії'][0]['Назва банку'].lower().translate(str.maketrans('', '', string.punctuation)).strip())) # remove all punctuation, double spaces and trail spaces, lowercase
+    
+    bname = banks2[i]['Ліцензії'][0]['Назва банку'].lower()
+    for c in string.punctuation:
+        bname = bname.replace(c, " ")
+    bname = re.sub(' +',' ', bname.strip())
+    
+    banks_names.append(bname) 
    
     # EDRPOU code
     banks_edrpou.append(str(banks2[i]['Ліцензії'][0]['Код ЄДРПОУ']))
 
-del banks1, banks2, i
+del i, c, bname
         
 # Additional processing
 
@@ -63,17 +83,23 @@ del len_banks_edrpou, i
 
 # We will add names of banks without all additional words
 
+banks_names = list(set(banks_names)) # remove duplicates names
+
 len_banks_names = len(banks_names)
 
 for i in range(len_banks_names):
-    if bool(re.search('філія|управління|пат|(публічне |)акціонерне товариство|(публічного |)акціонерного товариства|(а|)кб |акціонерний банк|^ат | ат ', \
+    if bool(re.search('№|0-9|твбв|філія|управління|пат|(публічне |)акціонерне товариство|(публічного |)акціонерного товариства|(а|)кб |акціонерний банк|^ат | ат ', \
                    banks_names[i])):
         banks_names.append(re.sub(' +', ' ', \
-                                  re.sub('філія|управління|пат|(публічне |)акціонерне товариство|(публічного |)акціонерного товариства|(а|)кб |акціонерний банк|^ат | ат ', \
-                                  '', banks_names[i])).strip())
+                                  re.sub('№|0-9|твбв|філія|управління|пат|(публічне |)акціонерне товариство|(публічного |)акціонерного товариства|(а|)кб |акціонерний банк|^ат | ат ', \
+                                  ' ', banks_names[i])).strip())
 
 banks_names = list(set(banks_names)) # remove duplicates of banks' names
 banks_names = list(filter(None, banks_names)) # remove empty strings
 banks_names = list(filter(lambda s: s not in ['0', 'None', 'банк', 'банки'], banks_names)) # remove empty strings and strings with just 'bank/s' word
 
 del len_banks_names, i
+
+# Removing very long names
+
+banks_names_short = [bname for bname in banks_names if len(bname) <= 30]
